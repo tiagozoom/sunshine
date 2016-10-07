53b4c641e84cf282ac9bdd9cd0d496ccea67210d
tiago <tiagozhk@gmail.com>
2016-10-06 21:45:26 -0300

    Implementing Contracts and tests

---
+++ app/src/androidTest/java/com/example/tgzoom/sunshine/ForecastParserUnitTest.java
@@ -0,0 +1,13 @@
+package com.example.tgzoom.sunshine;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * Created by tgzoom on 9/22/16.
+ */
+public class ForecastParserUnitTest {
+    public void parser_string() throws Exception {
+
+    }
+}
---
+++ app/src/main/java/com/example/tgzoom/sunshine/data/WeatherContract.java
@@ -0,0 +1,86 @@
+package com.example.tgzoom.sunshine.data;
+
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.net.Uri;
+import android.provider.BaseColumns;
+import android.text.format.Time;
+
+/**
+ * Created by tgzoom on 10/6/16.
+ */
+
+public class WeatherContract {
+
+    //Defining the authority that will be used throughout the application
+    public static final String CONTENT_AUTHORITY = "com.example.tgzoom.sunshine";
+
+    //Defining the base Uri to be used
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://"+CONTENT_AUTHORITY);
+
+    //Defining the possible paths for the application data
+    public static final String PATH_WEATHER  = "weather";
+    public static final String PATH_LOCATION = "location";
+
+    public static long normalizeDate(long startDate) {
+        // normalize the start date to the beginning of the (UTC) day
+        Time time = new Time();
+        time.set(startDate);
+        int julianDay = Time.getJulianDay(startDate, time.gmtoff);
+        return time.setJulianDay(julianDay);
+    }
+
+    public static final class LocationEntry implements BaseColumns{
+
+        //Table name
+        public static final String TABLE_NAME = "location";
+
+
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_LOCATION).build();
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_LOCATION;
+
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_LOCATION;
+
+        //Table Columns
+        public static final String COLUMN_LOCATION_SETTING = "location_setting";
+        public static final String COLUMN_CITY_NAME = "city_name";
+        public static final String COLUMN_COORD_LAT = "coord_lat";
+        public static final String COLUMN_COORD_LONG = "coord_long";
+
+        public static Uri buildLocationUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+    }
+
+    public static final class WeatherEntry implements BaseColumns{
+
+        //Table name
+        public static final String TABLE_NAME = "weather";
+
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_WEATHER).build();
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_WEATHER;
+
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_WEATHER;
+
+        public static final String COLUMN_LOC_KEY = "location_id";
+        public static final String COLUMN_DATE = "date";
+        public static final String COLUMN_MAX_TEMP = "max";
+        public static final String COLUMN_MIN_TEMP = "min";
+        public static final String COLUMN_HUMIDITY = "humidity";
+        public static final String COLUMN_PRESSURE = "pressure";
+        public static final String COLUMN_SHORT_DESC = "short_desc";
+        public static final String COLUMN_WIND_SPEED = "wind";
+        public static final String COLUMN_DEGREES = "degrees";
+        public static final String COLUMN_WEATHER_ID = "weather_id";
+
+        public static Uri buildWeatherUri(long id){
+            return ContentUris.withAppendedId(CONTENT_URI,id);
+        }
+    }
+}
